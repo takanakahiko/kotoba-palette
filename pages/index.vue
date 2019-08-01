a8a9722969cd712913865f6de8eafddf6fe096da
takanakahiko <takanakahiko@gmail.com>
2019-08-01 18:18:27 +0900

    fix share link

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
       <input type="text" placeholder="word" v-model="word" />
       <ColorsViewer :value="colors" />
       <div class="links">
-        <a href="https://twitter.com/intent/tweet?original_referer=http%3A%2F%2Flocalhost%3A3000%2F&ref_src=twsrc%5Etfw&text=ことばパレット%20%23kotoba-palette&tw_p=tweetbutton&url=https%3A%2F%2Fkotoba-palette.herokuapp.com%2F" target="_blank" class="button--green">
+        <a href="https://twitter.com/intent/tweet?original_referer=http%3A%2F%2Flocalhost%3A3000%2F&ref_src=twsrc%5Etfw&text=ことばパレット%20%23kotoba_palette&tw_p=tweetbutton&url=https%3A%2F%2Fkotoba-palette.herokuapp.com%2F" target="_blank" class="button--green">
           Share
         </a>
         <a href="https://github.com/takanakahiko/word2colors" target="_blank" class="button--grey" >
