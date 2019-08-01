7e0220dd8351766de1802dbbf543154562232730
takanakahiko <takanakahiko@gmail.com>
2019-08-01 19:40:35 +0900

    replace api from google to bing

--- api/index.ts
+++ api/index.ts
@@ -1,15 +1,21 @@
-import GoogleImages from 'google-images'
 import Vibrant from 'node-vibrant'
 import express from 'express'
+const ImageSearchAPIClient = require('azure-cognitiveservices-imagesearch');
+const CognitiveServicesCredentials = require('ms-rest-azure').CognitiveServicesCredentials;
+
+const credentials = new CognitiveServicesCredentials(process.env.API_KEY);
+const imageSearchApiClient = new ImageSearchAPIClient(credentials);
 
 const app = express()
 
 app.get('/', async (req, res) =>{
   try {
-    const client = new GoogleImages(process.env.CSE_ID, process.env.API_KEY);
-    const images = await client.search(req.query.word)
-    const pureImages = images.filter(img => img.type === 'image/jpeg' || img.type === 'image/png')
-    const palette = await Vibrant.from(pureImages[0].url).getPalette()
+    console.log('ho')
+    const imageResults = await imageSearchApiClient.imagesOperations.search(req.query.word);
+    console.log('wa')
+    if(imageResults == null) throw Error('image not found')
+    const pureImages = imageResults.value.filter(img => img.encodingFormat === 'jpeg' || img.encodingFormat === 'png')
+    const palette = await Vibrant.from(pureImages[0].contentUrl).getPalette()
     res.json(palette)
   } catch (error) {
     res.status(500)
