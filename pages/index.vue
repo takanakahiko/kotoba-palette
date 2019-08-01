7e7cdadb7d8c553ce28acf8a0c47c135197cb8ca
takanakahiko <takanakahiko@gmail.com>
2019-08-01 18:13:54 +0900

    add tweet link

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
       <input type="text" placeholder="word" v-model="word" />
       <ColorsViewer :value="colors" />
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
+        <a href="https://twitter.com/intent/tweet?original_referer=http%3A%2F%2Flocalhost%3A3000%2F&ref_src=twsrc%5Etfw&text=ことばパレット&tw_p=tweetbutton&url=https%3A%2F%2Fkotoba-palette.herokuapp.com%2F" target="_blank" class="button--green">
           Share(Getting Ready...)
         </a>
         <a href="https://github.com/takanakahiko/word2colors" target="_blank" class="button--grey" >
