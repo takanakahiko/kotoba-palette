b3e5871587c96826b1f6ee2e6656557d99d99378
takanakahiko <takanakahiko@gmail.com>
2019-08-01 18:11:59 +0900

    add github-ribbon position

--- components/ForkMeOnGithub.vue
+++ components/ForkMeOnGithub.vue
@@ -4,14 +4,10 @@
   </a>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-})
-</script>
-
 <style scoped>
 a {
   position: absolute;
+  top: 0;
+  left: 0;
 }
 </style>
