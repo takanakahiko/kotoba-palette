41bd09eb5cdff530b37635aa4c80623f10b7265c
takanakahiko <takanakahiko@gmail.com>
2019-08-03 23:58:23 +0900

    support OGP

--- api/index.ts
+++ api/index.ts
@@ -1,20 +1,70 @@
 import Vibrant from 'node-vibrant'
 import express from 'express'
-const ImageSearchAPIClient = require('azure-cognitiveservices-imagesearch');
-const CognitiveServicesCredentials = require('ms-rest-azure').CognitiveServicesCredentials;
-
-const credentials = new CognitiveServicesCredentials(process.env.API_KEY);
-const imageSearchApiClient = new ImageSearchAPIClient(credentials);
+import ImageSearchAPIClient from 'azure-cognitiveservices-imagesearch'
+import { CognitiveServicesCredentials } from 'ms-rest-azure'
+import cors from 'cors'
+import AWS from 'aws-sdk'
+import multer from 'multer'
 
 const app = express()
 
-app.get('/', async (req, res) =>{
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+
+const credentials = new CognitiveServicesCredentials(process.env.API_KEY!);
+const imageSearchApiClient = new ImageSearchAPIClient(credentials);
+
+app.get('/getColors', async (req, res) =>{
   try {
     const imageResults = await imageSearchApiClient.imagesOperations.search(req.query.word);
     if(imageResults == null || imageResults.value.length == 0) throw Error('image not found')
     const pureImages = imageResults.value.filter(img => img.encodingFormat === 'jpeg' || img.encodingFormat === 'png')
+    if(!pureImages[0].contentUrl) throw Error('image not found')
     const palette = await Vibrant.from(pureImages[0].contentUrl).getPalette()
     res.json(palette)
+  } catch (error) {
+    console.log(error.toString())
+    res.status(500)
+    res.json({error: error.toString()})
+  }
+})
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+});
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.post('/saveOgpImage', upload.single('image'), async (req, res) =>{
+  try {
+    const objectKey = Date.now().toString()
+    const params = {
+      Bucket: process.env.AWS_BUCKET_NAME!,
+      Key: objectKey,
+      Body: req.file.buffer,
+      ContentType: req.file.mimetype,
+      ACL: "public-read"
+    }
+    const ret = await s3.upload(params).promise();
+    res.json({id:objectKey})
+  } catch (error) {
+    console.log(error.toString())
+    res.status(500)
+    res.json({error: error.toString()})
+  }
+})
+
+app.get('/ogpImage', async (req, res) =>{
+  try {
+    const params = {
+      Bucket: process.env.AWS_BUCKET_NAME!,
+      Key: req.query.id
+    }
+    const ret = await s3.getObject(params).promise();
+    res.contentType(ret.ContentType!);
+    res.status(200).send(ret.Body);
   } catch (error) {
     console.log(error.toString())
     res.status(500)
--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
       <input type="text" placeholder="word" v-model="word" />
       <ColorsViewer :value="colors" />
       <div class="links">
-        <a href="https://twitter.com/intent/tweet?original_referer=http%3A%2F%2Flocalhost%3A3000%2F&ref_src=twsrc%5Etfw&text=ことばパレット%20%23kotoba_palette&tw_p=tweetbutton&url=https%3A%2F%2Fkotoba-palette.herokuapp.com%2F" target="_blank" class="button--green">
+        <a @click="share" target="_blank" class="button--green">
           シェア
         </a>
         <a href="https://twitter.com/takanakahiko" target="_blank" class="button--grey" >
@@ -19,6 +19,7 @@
 
 <script>
 import debounce from 'lodash/debounce'
+import twitterShare from 'twitter-share'
 
 import ColorsViewer from '~/components/ColorsViewer.vue'
 import ForkMeOnGithub from '~/components/ForkMeOnGithub.vue'
@@ -32,6 +33,7 @@ export default {
     return {
       word: '',
       colors: [],
+      id: null
     }
   },
   watch: {
@@ -42,16 +44,96 @@ export default {
   created: function(){
     this.debouncedGetColors = debounce(this.getColors, 500)
   },
+  asyncData: function({query}){
+    const hexToRgb = (hex) => {
+      const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+      return [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)]
+    }
+    return {
+      id : query.id,
+      word : query.word,
+      colors : (query.colors) ? query.colors.split(',').map(hexToRgb) : undefined
+    }
+  },
+  head () {
+    const meta = [
+      { hid: 'og:site_name', property: 'og:site_name', content: 'イベントページからツイッターのリストを作るやつ' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://kotoba-palette.herokuapp.com' },
+      { hid: 'og:title', property: 'og:title', content: 'イベントページからツイッターのリストを作るやつ' },
+      { hid: 'og:description', property: 'og:description', content: 'connpass の URL から Twitter のリストを自動生成します' },
+    ]
+    if(this.id){
+      meta.push({ hid: 'og:image', property: 'og:image', content: 'https://kotoba-palette.herokuapp.com/api/ogpImage?id=' + this.id })
+      meta.push({ name: 'twitter:card', content: 'summary_large_image' })
+    }
+    return {
+      title: 'ことばパレット',
+      meta: meta
+    }
+  },
   methods: {
     getColors: async function(){
       if(this.word.length == 0) return;
       this.$nuxt.$loading.start()
       try {
-        const ret = await fetch('/api?word=' + this.word)
+        const ret = await fetch('/api/getColors?word=' + this.word)
         const retJson = await ret.json()
         if(retJson.error) throw Error(retJson.error)
         this.colors = Object.keys(retJson).map(key => retJson[key].rgb)
       } catch (error) {
+        console.log(error)
+        this.$toast.error('エラーになりました : ' + error)
+      }
+      this.$nuxt.$loading.finish()
+    },
+    share: async function(){
+      const canvas = document.createElement("canvas")
+      canvas.height = 630
+      canvas.width = 1200
+      const ctx = canvas.getContext("2d")
+      ctx.fillStyle = `rgb(255,255,255)`
+      ctx.fillRect(0,0,1200,630);
+      ctx.fillStyle = `rgb(0,0,0)`
+      ctx.font = "150px sans-serif"
+      const wid = ctx.measureText(this.word)
+      ctx.fillText(this.word, Math.max((800-wid.width)/2+200,200), 250, 800)
+      const rgb2code = (rgb) => {
+        if(rgb[0] == -1) return ''
+        return "#" + Math.floor((1 << 24) + (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]).toString(16).slice(1);
+      }
+      ctx.font = "20px sans-serif"
+      for(var i=0;i<6;i++){
+        ctx.fillStyle = `rgb(${this.colors[i].join(',')})`;
+        ctx.fillRect(175+i*150,350,100,100);
+        ctx.fillStyle = `rgb(0,0,0)`
+        ctx.fillText(rgb2code(this.colors[i]), 175+i*150,480,100)
+      }
+      const formData = new FormData();
+      const getCanvasBlob = (canvas) => {
+        return new Promise((resolve, reject) => canvas.toBlob(resolve))
+      }
+      formData.append('image', await getCanvasBlob(canvas));
+      const options = {
+        method: 'POST',
+        body: formData
+      }
+      try {
+        const ret = await fetch('/api/saveOgpImage', options);
+        const retJson = await ret.json()
+        if(retJson.error) throw Error(retJson.error)
+        const rgb2hex = (rgb) => {
+          if(rgb[0] == -1) return ''
+          return Math.floor((1 << 24) + (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]).toString(16).slice(1);
+        }
+        const tweetLink = twitterShare({
+          text: 'ことばパレット',
+          url: `https://kotoba-palette.herokuapp.com?id=${retJson.id}&word=${this.word}&colors=${this.colors.map(rgb2hex).join(',')}`,
+          hashtags: ['kotoba_palette']
+        });
+        window.open(tweetLink)
+      } catch (error) {
+        console.log(error)
         this.$toast.error('エラーになりました : ' + error)
       }
       this.$nuxt.$loading.finish()
