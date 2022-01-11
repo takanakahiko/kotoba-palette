8ae395654988a31710cccec24e1cf7e7fa0b10be
takanakahiko <takanakahiko@gmail.com>
2022-01-12 00:36:00 +0900

    いろいろ直した

--- server-middleware/redirect.js
+++ server-middleware/redirect.js
@@ -1,7 +1,8 @@
 export default function (req, res, next) {
-  if(req.host == 'kotoba-palette.herokuapp.com'){
-    res.writeHead(301, { Location: `https://kotoba-palette.takanakahiko.me${req.originalUrl}` })
-    res.end()
+  if(req.headers.host == 'kotoba-palette.herokuapp.com'){
+    const url = new URL(req.originalUrl, 'https://kotoba-palette.takanakahiko.me')
+    res.writeHead(301, { Location: url })
+    return res.end()
   }
   next()
 }
