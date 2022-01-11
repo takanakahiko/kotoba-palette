4e55d5e8c9ade853ecac9d2c0c4e622ba171115a
takanakahiko <takanakahiko@gmail.com>
2022-01-11 23:37:54 +0900

    fix redirect middleware

--- server-middleware/redirect.js
+++ server-middleware/redirect.js
@@ -1,5 +1,5 @@
 export default function (req, res, next) {
-  if(req.get('host') == 'kotoba-palette.herokuapp.com'){
+  if(req.host == 'kotoba-palette.herokuapp.com'){
     const url = new URL(`https://kotoba-palette.takanakahiko.me${req.originalUrl}`)
     res.writeHead(301, { Location: redirect.to })
     res.end()
