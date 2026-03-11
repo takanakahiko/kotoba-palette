5430f43bfba572f7d0cf32dd31caa29eb5c0ceb5
takanakahiko <takanakahiko@gmail.com>
2026-03-11 17:39:13 +0900

    rename to kotonoha-palette

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1, maximum-scale=1",
-      title: "ことばパレット",
+      title: "ことのはパレット",
       meta: [{ name: "description", content: "あなたが好きなものは，どんな色をしてますか？" }],
       link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     },
